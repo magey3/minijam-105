568f5ff6755d7cad902953925c65669e69dc05d1
magey3 <--global>
2022-04-29 20:10:14 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+use crate::ldtk::*;
+use bevy::{asset::AssetServerSettings, prelude::*};
+use bevy_ecs_tilemap::prelude::*;
+use bevy::render::render_resource::TextureUsages;
+
+pub fn set_texture_filters_to_nearest(
+    mut texture_events: EventReader<AssetEvent<Image>>,
+    mut textures: ResMut<Assets<Image>>,
+) {
+    // quick and dirty, run this for all textures anytime a texture is created.
+    for event in texture_events.iter() {
+        match event {
+            AssetEvent::Created { handle } => {
+                if let Some(mut texture) = textures.get_mut(handle) {
+                    texture.texture_descriptor.usage = TextureUsages::TEXTURE_BINDING
+                        | TextureUsages::COPY_SRC
+                        | TextureUsages::COPY_DST;
+                }
+            }
+            _ => (),
+        }
+    }
+}
+
+mod ldtk;
+
+fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    let handle: Handle<LdtkMap> = asset_server.load("house.ldtk");
+
+    let map_entity = commands.spawn().id();
+
+    commands.entity(map_entity).insert_bundle(LdtkMapBundle {
+        ldtk_map: handle,
+        map: Map::new(0u16, map_entity),
+        transform: Transform::from_xyz(0.0, 0.0, 0.0),
+        ..Default::default()
+    });
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(WindowDescriptor {
+            width: 1270.0,
+            height: 720.0,
+            title: String::from("LDTK Example"),
+            ..Default::default()
+        })
+        .insert_resource(AssetServerSettings {
+            watch_for_changes: true,
+            ..default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(TilemapPlugin)
+        .add_plugin(LdtkPlugin)
+        .add_startup_system(startup)
+        .add_system(set_texture_filters_to_nearest)
+        .run();
 }
